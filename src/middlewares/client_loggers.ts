ba7f129cedee424ff260879d84446930f2b21181
Binh-2060 <binhxayxana@gmail.com>
2024-08-14 17:17:28 +0700

    main add latency in client loggers

--- src/middlewares/client_loggers.ts
+++ src/middlewares/client_loggers.ts
@@ -1,15 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import loggers from '../utils/loggers';
 
+const getDurationInMilliseconds = (start: [number, number]) => {
+  const NS_PER_SEC = 1e9;
+  const NS_TO_MS = 1e6;
+  const diff = process.hrtime(start);
+  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+};
+
 export const ClientHandlers = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   const originalSend = res.send;
+  const start = process.hrtime();
   res.send = function (body: any) {
     // Log response information
-
     loggers.info({
       message: 'Request recieved',
       method: req.method,
@@ -21,6 +28,7 @@ export const ClientHandlers = (
       request_id: req['requestId'],
       origin: 'api',
       response: JSON.parse(body),
+      latency: `${getDurationInMilliseconds(start).toLocaleString()} ms`,
     });
 
     // Call the original send function
